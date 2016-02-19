2c4c8af33bffbcda27513c26bcd75df4d5ac96a0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-02-19 18:03:49 +0100

    Tested some proprocessing

---
+++ src/preprocess.jl
@@ -0,0 +1,16 @@
+"""Align matrix such that in each row the maximum has the position of the maximum of the mean row"""
+function align_max{T <: Number}(S::Matrix{T})
+    b = mean(S, 1)[:] # compute mean row
+    _, max_b = findmax(b)
+    ncols = size(S, 2)
+    A = Array(eltype(S), size(S)) # output array
+    for i = 1:size(A, 1)
+        _, max_i = findmax(S[i, :])
+        offset = max_b - max_i
+        for j = 1:size(A, 2)
+            k = (j+offset+ncols-1) % ncols + 1
+            A[i,j] = S[i,k]
+        end
+    end
+    return A
+end
