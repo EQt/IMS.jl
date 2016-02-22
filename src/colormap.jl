0afa0ee69f00f189a57fd71c5e5a9cfbc0cc1705
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-02-22 13:12:22 +0100

    make it more general

--- src/colormap.jl
+++ src/colormap.jl
@@ -25,9 +25,9 @@ cm = ColorMap("ims", r, g, b)
 register_cmap("ims", cm)
 
 """Show a measurement matrix using an appropriate colormap"""
-imsshow(s::Matrix{Float64}; vmin=0, vmax=100) =
+imsshow{T <: Number}(s::Matrix{T}; vmin=0, vmax=100) =
     imshow(s; cmap="ims", vmin=vmin, vmax=vmax, interpolation="none", origin="lower")
 
 """Like `imshow` but save an image using the `imsave` method of `PyPlot`"""
-imssave(s::Matrix{Float64}, f::AbstractString) =
+imssave{T <: Number}(s::Matrix{T}, f::AbstractString) =
     imsave(f, s; cmap="ims", vmin=0, vmax=100, origin="lower")
