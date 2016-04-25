22cb15b0b9e3a5a53eb10a7f20c3ab9e2fb0be3e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2016-04-25 19:26:55 +0200

    be more flexible

--- src/peaks.jl
+++ src/peaks.jl
@@ -1,7 +1,7 @@
 using DataFrames
 
 """Print in the format we used in the project group"""
-function write_peaks(path::AbstractString, df::DataFrame)
+function write_peaks(path::Union{AbstractString,IO}, df::DataFrame)
     d = copy(df)
     if !(:name in names(d))
         d[:name] = [symbol(@sprintf("P%d", i)) for i=1:size(d, 1)]
@@ -9,7 +9,11 @@ function write_peaks(path::AbstractString, df::DataFrame)
     end
     # d[:t] .*= 100
     sort!(d, cols=order(:r))
-    writetable(path, d, separator='\t', header=true)
+    if isa(path, IO)
+        printtable(path, d, separator='\t', header=true)
+    else
+        writetable(path, d, separator='\t', header=true)
+    end
 end
 
 
